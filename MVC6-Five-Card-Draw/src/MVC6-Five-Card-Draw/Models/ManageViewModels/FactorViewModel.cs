dbb182b7a9b8abc6a76bdbf3cc9f4a1cc5002cbf
Tamer Rifai <tjrifai@gmail.com>
2016-05-21 23:23:25 -0400

    Just the initial check in using the ASP.NET Core Web Application template with individual user accounts.

---
+++ MVC6-Five-Card-Draw/src/MVC6-Five-Card-Draw/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC6_Five_Card_Draw.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
